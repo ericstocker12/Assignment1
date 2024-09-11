ac7a6027e674e08b283c4ce67e8286414ca0b1f5
ericstocker12 <eric.stocker@csuglobal.edu>
2024-09-11 15:51:25 -0500

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,18 @@
+Python 3.12.6 (v3.12.6:a4a2d2b0d85, Sep  6 2024, 16:08:03) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1=12
+>>> num2=10
+>>> result_add=num1+num2
+>>> result_subtract=num1-num2
+>>> print(result_add)
+22
+>>> print(result_subtract)
+2
+>>> //Part 2//
+SyntaxError: invalid syntax
+>>> result_multiply= num1 *num2
+>>> result_divide= num1/ num2
+>>> print(result_multiply)
+120
+>>> print(result_divide)
+1.2
